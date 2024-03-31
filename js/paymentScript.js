b9a71315299e064f6bc320cfb036d88032c31bae
YERAY <yeray.lois@udc.es>
2024-03-31 09:03:28 +0200

    Correction Script Validation Payment

--- js/paymentScript.js
+++ js/paymentScript.js
@@ -15,19 +15,32 @@ document.getElementById('cardNumber').addEventListener('input', function(e) {
     e.target.value = finalValue;
 });
 
-
 // Formato automático para la fecha de caducidad
 document.getElementById('expiryDate').addEventListener('input', function(e) {
     var input = e.target.value;
-    input = input.replace(/\D/g, '').slice(0, 4); // Asegurar solo dígitos y longitud máxima
+    var oldVal = e.target.getAttribute('data-old-value') || '';
 
-    if (input.length > 2) {
-        input = input.slice(0, 2) + '/' + input.slice(2, 4);
+    // Asegurar solo dígitos y longitud máxima
+    input = input.replace(/\D/g, '').slice(0, 4);
+
+    // Agregar barra si la longitud es adecuada y el usuario no está borrando
+    if (input.length >= 2) {
+        // Comprobar si el usuario está intentando borrar la barra
+        if(oldVal.length > input.length && oldVal[oldVal.length - 1] === '/') {
+            input = input.slice(0, 1); // Permitir borrar la barra
+        } else {
+            input = input.slice(0, 2) + '/' + input.slice(2, 4);
+        }
     }
+
+    // Actualizar el valor del campo
     e.target.value = input;
+
+    // Almacenar el valor actual para la próxima comparación
+    e.target.setAttribute('data-old-value', input);
 });
 
-// Validación de la fecha de caducidad al intentar enviar el formulario
+
 document.querySelector('form').addEventListener('submit', function(e) {
     const expiryInput = document.getElementById('expiryDate');
     const expiryValue = expiryInput.value;
@@ -35,12 +48,20 @@ document.querySelector('form').addEventListener('submit', function(e) {
     const currentYear = new Date().getFullYear() % 100; // Obtener los últimos dos dígitos del año actual
     const currentMonth = new Date().getMonth() + 1; // getMonth() devuelve un índice basado en cero, por lo que sumamos 1
 
+    // Comprobar si el mes es mayor que 12 o menor que 1
+    if (month > 12 || month < 1) {
+        alert('El mes ingresado no es válido. Por favor, ingrese un mes válido (entre 1 y 12).');
+        e.preventDefault(); // Detener el envío del formulario
+        return;
+    }
+
     // Comprobar si la fecha de caducidad es anterior a la fecha actual
     if (year < currentYear || (year === currentYear && month < currentMonth)) {
         alert('Fecha de caducidad de la tarjeta no es válida. Por favor, compruebe nuevamente.');
         e.preventDefault(); // Detener el envío del formulario
     }
 });
+
 
 // Validación del número de tarjeta con el algoritmo de Luhn
 function isValidCardNumber(number) {
