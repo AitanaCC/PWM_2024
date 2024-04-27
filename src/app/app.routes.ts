42bc9558e05ffbf013b5cd0dbd966d905c8c07bd
AitanaCC <aitana.carreno101@alu.ulpgc.es>
2024-04-27 14:19:08 +0100

    creacion de la pagina contact-us y un poco de payment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {RegisterLoginComponent} from "./pages/register-login/register-login.component";
 import {AppComponent} from "./app.component";
+import {ContactUsComponent} from "./pages/contact-us/contact-us.component";
+import {PaymentComponent} from "./pages/payment/payment.component";
 
 export const routes: Routes = [
 
   {path: '', redirectTo:'Home', pathMatch:'full'},
   {path: 'Home', component:HomeComponent},
   {path: 'register-login', component:RegisterLoginComponent},
-
+  {path: 'contact-us', component:ContactUsComponent},
+  {path: 'payment', component:PaymentComponent},
 ];
---
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-contact-us',
+  standalone: true,
+    imports: [
+        HeaderComponent
+    ],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.css'
+})
+export class ContactUsComponent {
+  protected contactEmail?:String
+  contructor(){
+    this.contactEmail="email@example.com"
+  }
+
+}
